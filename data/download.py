19753eebcd06e8d8ccfee4abc4bc8c157098e6ec
Ahmed Al-Dulimi <ahmed.aldulimi@student.unimelb.edu.au>
2022-04-19 01:05:13 +1000

    added logging to the download file

--- data/download.py
+++ data/download.py
@@ -6,6 +6,10 @@ import aiohttp
 import random
 import xml.dom.minidom
 import xml.etree.ElementTree as ET
+import logging 
+
+logging.basicConfig(filename='log/download.log',level=logging.INFO,
+    format='%(asctime)s:%(levelname)s:%(message)s')
 
 headers = {
     'user-agent': f'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.{random.randint(0, 9999)} Safari/537.{random.randint(0, 99)}'  
@@ -16,7 +20,7 @@ all_days = []
 base_url = 'https://www.nytimes.com/sitemap/2021/'
 response = requests.get(base_url, headers=headers)
 
-with open("data/data.xml", 'w') as file:
+with open("data/data2.xml", 'w') as file:
     file.write('<documents>\n')
     file.close()
 
@@ -28,63 +32,73 @@ for node in HTMLParser(response.text).css('div > ol > li'):
         day_url = url + node.child.attrs['href']
         all_days.append(day_url)
 
+logging.info(f'365 day urls fetched successfully')
 
-article_count = 0
+article_count = 1
+article_success = 1
 to_load = []
 async def get_article_data(session, url):
-    async with session.get(url) as resp:
-        response_text = await resp.text()
-        
-        if response_text:            
-            article = HTMLParser(response_text)
-
-            title = article.css_first('h1')
-            if title == None: return 
-            title = title.text()
-
-            try:
-                date = article.css_first('time')
-                date = date.attributes['datetime']
-                if 'Z' in date: date = date[:date.find('.000Z')] + '+00:00'
-                date = datetime.datetime.fromisoformat(date)
-                date = date.strftime('%Y-%m-%d %H:%M:%S')
-
-            except:
-                date = ''
-
-            author = article.css_first('span.last-byline')
-            if author == None: author = 'unkown' 
-            else: author = author.child.text()
-
-            body = ''
-            for node in article.css('div.StoryBodyCompanionColumn'):
-                paragraph = node.child.select('p').matches
-                for p in paragraph:
-                    if p: body += p.text()
-
-            doc = ET.Element('doc')
-            ET.SubElement(doc, "title").text = title
-            ET.SubElement(doc, "author").text = author
-            ET.SubElement(doc, "body").text = body
-            ET.SubElement(doc, "datetime").text = date
-            ET.SubElement(doc, "url").text = url
-
-            dom = xml.dom.minidom.parseString(ET.tostring(doc))
-            doc = dom.childNodes[0].toprettyxml()
-
-            global to_load, article_count
-            to_load.append(doc)
+    try:
+        async with session.get(url) as resp:
+            response_text = await resp.text()
             
-            if len(to_load) >= 10:
-                with open("data/data.xml", 'a') as file:
-                    for doc in to_load:
-                        file.write(doc)
+            if response_text:            
+                article = HTMLParser(response_text)
 
-                    file.close()
+                title = article.css_first('h1')
+                if title == None: return 
+                title = title.text()
 
-                to_load = []
+                try:
+                    date = article.css_first('time')
+                    date = date.attributes['datetime']
+                    if 'Z' in date: date = date[:date.find('.000Z')] + '+00:00'
+                    date = datetime.datetime.fromisoformat(date)
+                    date = date.strftime('%Y-%m-%d %H:%M:%S')
+
+                except:
+                    date = ''
+
+                author = article.css_first('span.last-byline')
+                if author == None: author = 'unkown' 
+                else: author = author.child.text()
+
+                body = ''
+                for node in article.css('div.StoryBodyCompanionColumn'):
+                    paragraph = node.child.select('p').matches
+                    for p in paragraph:
+                        if p: body += p.text()
+
+                doc = ET.Element('doc')
+                ET.SubElement(doc, "title").text = title
+                ET.SubElement(doc, "author").text = author
+                ET.SubElement(doc, "body").text = body
+                ET.SubElement(doc, "datetime").text = date
+                ET.SubElement(doc, "url").text = url
+
+                dom = xml.dom.minidom.parseString(ET.tostring(doc))
+                doc = dom.childNodes[0].toprettyxml()
+
+                global to_load, article_count, article_success
+                to_load.append(doc)
                 
-            article_count += 1
+                if len(to_load) >= 10:
+                    with open("data/data2.xml", 'a') as file:
+                        for doc in to_load:
+                            file.write(doc)
+
+                        file.close()
+
+                    to_load = []
+                
+                logging.info(f'{article_success}/{article_count} Downloaded successfully {title}')
+
+                article_success += 1
+                article_count += 1
+            return 
+    except:
+        article_count += 1
+        logging.warning(f'{article_count - article_success} Download failed')
         return 
 
 async def get_article_url(session, url):
@@ -105,7 +119,7 @@ async def get_article_url(session, url):
 async def main():
     async with aiohttp.ClientSession(headers=headers) as session:
         tasks = []
-        for day_url in all_days[:10]: 
+        for day_url in all_days[:60]: 
             tasks.append(asyncio.ensure_future(get_article_url(session, day_url)))
         
         collection = await asyncio.gather(*tasks)
@@ -114,6 +128,6 @@ async def main():
 asyncio.run(main())
 
 
-with open("data/data.xml", 'a') as file:
+with open("data/data2.xml", 'a') as file:
     file.write('</documents>')
     file.close()
